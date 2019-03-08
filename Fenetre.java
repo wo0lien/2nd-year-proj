6b7de90159c346a63efba946859c8828f1b4c263
Tom <tom.sampic26@gmail.com>
2019-03-08 13:10:53 +0100

    Ajout creation de planete

--- Fenetre.java
+++ Fenetre.java
@@ -7,17 +7,18 @@ import java.awt.AWTException;
 /**
  * Fenetre
  */
-public class Fenetre extends JFrame implements KeyListener{
+public class Fenetre extends JFrame implements ActionListener, KeyListener {
 
     //variables
 
-    private JPanel mainPanel;
+    private JPanel mainPanel, footerPanel;
+    private JButton newPlanet;
     private Space space;
 
     public Fenetre(String nom) throws AWTException{
         super(nom);
 
-        this.setSize(1000, 700);
+        this.setSize(1000, 850);
 
         //Jpanel principal (content pane)
         mainPanel = new JPanel();
@@ -25,25 +26,50 @@ public class Fenetre extends JFrame implements KeyListener{
         mainPanel.setBackground(Color.black);
 
         //création du JPanel Space a la taille de la fenetre
-        space = new Space(this.getInsets().left , this.getInsets().top, this.getWidth() - this.getInsets().left - this.getInsets().right, this.getHeight() - this.getInsets().top - this.getInsets().bottom);
+        space = new Space(this.getInsets().left , this.getInsets().top, this.getWidth() - this.getInsets().left - this.getInsets().right, this.getHeight() - this.getInsets().top - this.getInsets().bottom - 150);
         mainPanel.add(space);
+
+        //panel de bas de page
+
+        footerPanel = new JPanel();
+        footerPanel.setLayout(null);
+        footerPanel.setBounds(this.getInsets().left, this.getHeight() - this.getInsets().top - this.getInsets().bottom - 150, this.getWidth() - this.getInsets().left - this.getInsets().right, 150);
+        footerPanel.setBackground(Color.WHITE);
+
+        newPlanet = new JButton("Nouvelle planete");
+        newPlanet.setBounds(20, 20, 200, 60);
+
+        footerPanel.add(newPlanet);
+
+        mainPanel.add(footerPanel);
 
         //ajout du panel à la fenetre principale
         this.setContentPane(mainPanel);
 
         //ajout des listeners
         this.addKeyListener(this);
+        newPlanet.addActionListener(this);
 
         //affichage de la fenetre et arret a la fermeture
         this.setVisible(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-    //keyEvents pour la suite
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == newPlanet) {
+            //on lance la methode newPlanet
+            space.NewPlanet();
+        }
+    }
+
+    // keyEvents pour la suite
     @Override
     public void keyPressed(KeyEvent e) {}
     @Override
     public void keyReleased(KeyEvent e) {}
     @Override
     public void keyTyped(KeyEvent e) {}
+
 }
--- Space.java
+++ Space.java
@@ -1,26 +1,35 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
+
 /**
  * Space
  */
-public class Space extends JPanel implements ActionListener {
+public class Space extends JPanel implements ActionListener, MouseListener, MouseMotionListener {
 
-    //objets
-    private Soleil s;
-    private Planete p;
-
-    //timer
+    // timer
     Timer t;
     private int dt = 40, temps = 0;
 
-    //Image pour l'affichage sans scintillements
+    // variable de modes en fonction des actions de l'utilisateur mode 0 normal,
+    // mode 1 nouvelle planete
+
+    private int mode = 0;
+    private boolean mouseIn = false;
+    private int mouseX = 0, mouseY = 0;
+    private int newPlanetX =  0, newPlanetY = 0, newPlanetRadius = 20;
+
+    // Image pour l'affichage sans scintillements
     private BufferedImage monBuf; // buffer d’affichage
 
     private BufferedImage spaceStars;
@@ -28,43 +37,54 @@ public class Space extends JPanel implements ActionListener {
     public Space(int xPos, int yPos, int x, int y) {
 
         System.out.println("Create the universe");
-	
+
         this.setLayout(null);
         this.setBounds(xPos, yPos, x, y);
 
         t = new Timer(dt, this);
         t.start();
 
-        //objets
-
-        s = new Soleil(100, 300, 300);
-        p = new Planete(100, 0.005, 450, 300, s);
-
         Dimension dim = getSize();
-        //buffered image
+
+        // buffered image
         monBuf = new BufferedImage(dim.width, dim.height, BufferedImage.TYPE_INT_RGB);
 
-        //image du fond
+        // image du fond
 
         try {
             spaceStars = ImageIO.read(getClass().getResourceAsStream("space_with_stars.png"));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        repaint();
 
+        // ajout des listeners
+
+        this.addMouseListener(this);
+        this.addMouseMotionListener(this);
+
+        repaint();
 
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        temps += dt;
-        repaint();
+        if (e.getSource() == t) {
+            temps += dt;
+            repaint();
+        }
     }
-    
+
+    /**
+     * Methode pour entrer dans le mode de création d'une planete
+     */
+    public void NewPlanet() {
+        System.out.println("Passage en mode ajout de planete");
+        mode = 1;
+    }
+
     /**
      * Methode d'affichage des elements visuels
+     * 
      * @params graphics g
      */
     @Override
@@ -72,23 +92,123 @@ public class Space extends JPanel implements ActionListener {
 
         Prepaint(monBuf.getGraphics());
         g.drawImage(monBuf, getInsets().left, getInsets().top, null);
-        
+
     }
 
     /**
      * Méthode pour précharger l'affichage et éviter les scintillements
      */
-    public void Prepaint(Graphics g){   
+    public void Prepaint(Graphics g) {
 
-        //draw background image
+        // draw background image
 
         g.drawImage(spaceStars, 0, 0, null);
 
-        
-        //paint objets
-        s.paint(g, temps);
-        p.paint(g, temps);
+        // prise en compte du mode
 
+        switch (mode) {
+        case 0:
+            // affichage classique de l'animation
+            break;
+        case 1:
+            // affichage de la planete sur le curseur de l'utilisateur
+            if (mouseIn) {
+                g.setColor(Color.green);
+                g.fillOval(mouseX - newPlanetRadius / 2, mouseY - newPlanetRadius / 2, newPlanetRadius, newPlanetRadius);
+            }
+        case 2:
+            //position de la nouvelle planete fixee => fixage de la taille
+            if (mouseIn) {
+                g.setColor(Color.green);
+                g.fillOval(newPlanetX - newPlanetRadius / 2, newPlanetY - newPlanetRadius / 2, newPlanetRadius, newPlanetRadius);
+            }
+            break;
+        default:
+            break;
+        }
+
+    }
+
+    //mouse motion methods
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        mouseX = e.getX();
+        mouseY = e.getY();
+
+        switch (mode) {
+            case 0:
+                
+                break;
+        
+            case 1:
+                
+                break;
+            case 2:
+                //mode fixage de la taille de la planete
+                newPlanetRadius = (int)Math.sqrt(Math.pow(mouseX - newPlanetX, 2) + Math.pow(mouseY - newPlanetY, 2));
+                break;
+            
+            default:
+                break;
+        }
+    }
+
+    //mouse methods
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        switch (mode) {
+            case 0:
+                //chose qui se passe quand le jeu est en mode normal
+                break;
+            case 1:
+                //fixage de la position de la nouvelle planete
+                newPlanetX = mouseX;
+                newPlanetY = mouseY;
+                //passage au mode suivant
+                mode = 2;
+                break;
+
+            case 2:
+                //sauvegarde de la planete dans la liste des objets
+
+                //repassage en mode 0 et réinitialisation
+
+                mode = 0;
+
+                newPlanetRadius = 20;
+                newPlanetX = 0;
+                newPlanetY = 0;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        mouseIn = true;
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        mouseIn = false;
     }
 
 }
