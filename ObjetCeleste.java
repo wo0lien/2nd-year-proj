f252cb4d7269c9830442f27a72a261347d2d65ea
Leonard <leonard.grynfogel@gmail.com>
2019-03-12 08:18:53 +0100

    update ObjetCeleste.java

--- ObjetCeleste.java
+++ ObjetCeleste.java
@@ -1,9 +1,9 @@
 import java.awt.Graphics;
 import java.awt.Image;
+import java.util.LinkedList;
 
 /**
- * ObjetCeleste
- * Premiere instance abstract du composant de base objet celeste
+ * ObjetCeleste Premiere instance abstract du composant de base objet celeste
  */
 public abstract class ObjetCeleste {
 
@@ -20,15 +20,35 @@ public abstract class ObjetCeleste {
         dx = vx;
         dy = vy;
         disp = i;
+        for (int j = 0; j < objets.size(); j++) {
+            // int Distx;
+            // int Disty;
+            double r;
+            double Force;
+            double angle;
+            double dirx;
+            double diry;
+            dx = objets.get(j).x - x;
+            dy = objets.get(j).y - y;
+            r = dx * dx + dy * dy;
+            if (r != 0) {
+                Force = objets.get(j).masse / r;
+                angle = Math.atan2(dy, dx);
+                dirx = Force * Math.cos(angle);
+                diry = Force * Math.sin(angle);
+                vx += dirx;
+                vy += diry;
+            }
+        }
     }
 
-    //déclatation des méthodes abstract
+    // déclatation des méthodes abstract
 
     public abstract void paint(Graphics g);
 
     public abstract void update(int dt);
 
-    public int GetX(){
+    public int GetX() {
         return this.x;
     }
 
@@ -37,5 +57,8 @@ public abstract class ObjetCeleste {
     }
 
     public abstract void setVitesseX(double vx);
+
     public abstract void setVitesseY(double vy);
+
+    private LinkedList<ObjetCeleste> objets;
 }
