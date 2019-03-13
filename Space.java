4b3ea0ab4ae4e7d53cd646d844c3219e657688f5
Leonard <leonard.grynfogel@gmail.com>
2019-03-13 18:15:50 +0100

    Tentative évolution progressive

--- Space.java
+++ Space.java
@@ -34,7 +34,7 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
     private int mode = 0;
     private boolean mouseIn = false;
     private int mouseX = 0, mouseY = 0;
-    private int newPlanetX =  0, newPlanetY = 0, newPlanetRadius = 20;
+    private int newPlanetX = 0, newPlanetY = 0, newPlanetRadius = 20;
 
     // Image pour l'affichage sans scintillements
     private BufferedImage monBuf; // buffer d’affichage
@@ -43,14 +43,14 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
     private Image planetImage;
     private Image resizedPlanet;
 
-    //explosion
-    private Image explosion, resizedExplosion; 
+    // explosion
+    private Image explosion, resizedExplosion;
     private int explosionX, explosionY, explosionCounter, explosionR;
     private boolean explosing;
 
-    //objects
+    // objects
 
-    private LinkedList<ObjetCeleste> objets; 
+    private LinkedList<ObjetCeleste> objets;
 
     public Space(int xPos, int yPos, int x, int y) {
 
@@ -71,16 +71,16 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
         // images
 
         try {
-            
-            //chemins de fichier
+
+            // chemins de fichier
 
             File pathToSpace = new File("space_with_stars.png");
             File pathToPlanet = new File("earth.png");
-            
-            //transformation en objet image des fichier    
+
+            // transformation en objet image des fichier
             spaceStars = ImageIO.read(pathToSpace);
             planetImage = ImageIO.read(pathToPlanet);
-        
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -90,7 +90,7 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
             e.printStackTrace();
         }
 
-        //resize explosion
+        // resize explosion
         resizedExplosion = explosion.getScaledInstance(newPlanetRadius * 2, newPlanetRadius * 2, Image.SCALE_FAST);
         resizedPlanet = planetImage.getScaledInstance(newPlanetRadius * 2, newPlanetRadius * 2, Image.SCALE_FAST);
 
@@ -127,35 +127,40 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
                             angle = Math.atan2(dy, dx);
                             objet.vx += Force * Math.cos(angle);
                             objet.vy += Force * Math.sin(angle);
-                            
-                            if (Math.sqrt(r) < objet.r + obj.r && obj.masse < objet.masse){
+
+                            if (Math.sqrt(r) + 3 < objet.r + obj.r && obj.masse < objet.masse) {
                                 System.out.println("Collision");
                                 objet.r += obj.r;
                                 objet.masse += obj.masse;
-                                
-                                //animation de l'explosion
+
+                                // animation de l'explosion
 
                                 explosionX = obj.GetX();
                                 explosionY = obj.GetY();
                                 explosionCounter = 0;
 
                                 explosionR = obj.r;
-                                
-                                //resize Image
-                                resizedExplosion = explosion.getScaledInstance(explosionR * 2, explosionR * 2, Image.SCALE_FAST);
 
-                                //active l'affichage
+                                // resize Image
+                                resizedExplosion = explosion.getScaledInstance(explosionR * 2, explosionR * 2,
+                                        Image.SCALE_FAST);
+
+                                // active l'affichage
                                 explosing = true;
 
-                                //on supprime l'objet 
+                                // on supprime l'objet
                                 objets.remove(obj);
+                                /*
+                                 * if (objet.r < objet.r + explosionR) { objet.r += 1; }
+                                 */
                                 objet.resize();
-                    
+
                             }
                         }
+
                     }
-                objet.update(dt);
-                }     
+                    objet.update(dt);
+                }
             }
 
             repaint();
@@ -168,7 +173,7 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
     public void NewPlanet() {
         System.out.println("Passage en mode ajout de planete");
 
-        //reinitialisation des variables necessaires
+        // reinitialisation des variables necessaires
 
         newPlanetRadius = 20;
         newPlanetX = 0;
@@ -178,11 +183,10 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
         mode = 1;
     }
 
-
     public void TimerButton() {
-        
-        //changement d'etat de la variable pause
-        
+
+        // changement d'etat de la variable pause
+
         pause = !pause;
     }
 
@@ -207,8 +211,8 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
         // draw background image
 
         g.drawImage(spaceStars, 0, 0, null);
-        
-        //affichage de la liste des objets
+
+        // affichage de la liste des objets
 
         for (ObjetCeleste obj : objets) {
             obj.paint(g);
@@ -223,13 +227,13 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
         case 1:
             // affichage de la planete sur le curseur de l'utilisateur
             if (mouseIn) {
-                g.drawImage(resizedPlanet , mouseX - newPlanetRadius, mouseY - newPlanetRadius, null);
+                g.drawImage(resizedPlanet, mouseX - newPlanetRadius, mouseY - newPlanetRadius, null);
             }
             break;
         case 2:
-            //position de la nouvelle planete fixee => fixage de la taille
+            // position de la nouvelle planete fixee => fixage de la taille
             if (mouseIn) {
-                g.drawImage(resizedPlanet , newPlanetX - newPlanetRadius, newPlanetY - newPlanetRadius, null);
+                g.drawImage(resizedPlanet, newPlanetX - newPlanetRadius, newPlanetY - newPlanetRadius, null);
             }
             break;
         case 3:
@@ -242,20 +246,20 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
             break;
         }
 
-        //explosion
-        if (explosionCounter > 25){
+        // explosion
+        if (explosionCounter > 25) {
             explosing = false;
         }
         if (explosing) {
             explosionCounter++;
 
-            g.drawImage(resizedExplosion, explosionX - explosionR , explosionY - explosionR, null);
+            g.drawImage(resizedExplosion, explosionX - explosionR, explosionY - explosionR, null);
 
-        } 
+        }
 
     }
 
-    //mouse motion methods
+    // mouse motion methods
 
     @Override
     public void mouseDragged(MouseEvent e) {
@@ -268,68 +272,70 @@ public class Space extends JPanel implements ActionListener, MouseListener, Mous
         mouseY = e.getY();
 
         switch (mode) {
-            case 0:
-                
-                break;
-        
-            case 1:
-                
-                break;
-            case 2:
-                //mode fixage de la taille de la planete
-                newPlanetRadius = (int)Math.sqrt(Math.pow(mouseX - newPlanetX, 2) + Math.pow(mouseY - newPlanetY, 2));
+        case 0:
 
-                //mise a l'echelle de la planete
-                resizedPlanet = planetImage.getScaledInstance(newPlanetRadius * 2, newPlanetRadius * 2, Image.SCALE_FAST);
+            break;
 
-                break;
-            
-            default:
-                break;
+        case 1:
+
+            break;
+        case 2:
+            // mode fixage de la taille de la planete
+            newPlanetRadius = (int) Math.sqrt(Math.pow(mouseX - newPlanetX, 2) + Math.pow(mouseY - newPlanetY, 2));
+
+            // mise a l'echelle de la planete
+            resizedPlanet = planetImage.getScaledInstance(newPlanetRadius * 2, newPlanetRadius * 2, Image.SCALE_FAST);
+
+            break;
+
+        default:
+            break;
         }
     }
 
-    //mouse methods
+    // mouse methods
 
     @Override
     public void mouseClicked(MouseEvent e) {
         switch (mode) {
-            case 0:
-                //chose qui se passe quand le jeu est en mode normal
-                break;
-            case 1:
-                //fixage de la position de la nouvelle planete
-                newPlanetX = e.getX();
-                newPlanetY = e.getY();
-                //passage au mode suivant
-                System.out.println("Passe au mode selection taille");
-                mode = 2;
-                break;
+        case 0:
+            // chose qui se passe quand le jeu est en mode normal
+            break;
+        case 1:
+            // fixage de la position de la nouvelle planete
+            newPlanetX = e.getX();
+            newPlanetY = e.getY();
+            // passage au mode suivant
+            System.out.println("Passe au mode selection taille");
+            mode = 2;
+            break;
 
-            case 2:
-                //sauvegarde de la planete dans la liste des objets
-                //remplacer le 2 par un coef en fonction des materiaux
-                Planete newp = new Planete((double)3000 * newPlanetRadius, 0, 0, newPlanetX, newPlanetY, resizedPlanet, newPlanetRadius);
-                objets.add(newp);
+        case 2:
+            // sauvegarde de la planete dans la liste des objets
+            // remplacer le 2 par un coef en fonction des materiaux
+            Planete newp = new Planete((double) 750 * newPlanetRadius, 0, 0, newPlanetX, newPlanetY, resizedPlanet,
+                    newPlanetRadius);
+            objets.add(newp);
 
-                //repassage en mode 3
-                System.out.println("Passage au mode selection vitesse");
-                mode = 3;
-                break;
-            case 3:
+            // repassage en mode 3
+            System.out.println("Passage au mode selection vitesse");
+            mode = 3;
+            break;
+        case 3:
 
-                // on assigne a la planete la vitesse en x et en y en fonction de la position du curseur
+            // on assigne a la planete la vitesse en x et en y en fonction de la position du
+            // curseur
 
-                objets.get(objets.size() - 1).setVitesseX((double)e.getX() - newPlanetX);
-                objets.get(objets.size() - 1).setVitesseY((double)e.getY() - newPlanetY);
-                
-                //retour au mode 0
+            objets.get(objets.size() - 1).setVitesseX((double) e.getX() - newPlanetX);
+            objets.get(objets.size() - 1).setVitesseY((double) e.getY() - newPlanetY);
 
-                mode = 0;
+            // retour au mode 0
 
-                break;
-            default:
-                break;
+            mode = 0;
+
+            break;
+        default:
+            break;
         }
     }
 
