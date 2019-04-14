e0b4d4ff7fa17334ce4268c2754ad1ce6977b495
wo0lien <tom.sampic@insa-lyon.fr>
2019-04-14 12:14:56 +0200

    add exitButton

--- Fenetre.java
+++ Fenetre.java
@@ -20,7 +20,7 @@ public class Fenetre extends JFrame implements ActionListener {
 
     private static final int _400 = 400;
 	private JPanel mainPanel, footerPanel, hud;
-    private JButton newPlanet, timerButton;
+    private JButton newPlanet, timerButton, exitButton;
     private JTextArea timeCountJours, timeCountYears;
     private Space space;
 
@@ -72,14 +72,18 @@ public class Fenetre extends JFrame implements ActionListener {
         timerButton = new JButton("Start animation");
         timerButton.setBounds(250, 20, 200, 60);
 
+        exitButton = new JButton("Exit the game");
+        exitButton.setBounds((int)screenSize.getWidth() - 250, 20, 200, 60);
+
         timeCountJours = new JTextArea("Jours : " + space.getTempsJours());
-        timeCountJours.setBounds(480,40,200,60);
+        timeCountJours.setBounds(480, 40,200,60);
 
         timeCountYears = new JTextArea("Années : " + space.getTempsAnnées());
-        timeCountYears.setBounds(480,60,200,60);
-
+        timeCountYears.setBounds(480, 60,200,60);
+        
         footerPanel.add(newPlanet);
         footerPanel.add(timerButton);
+        footerPanel.add(exitButton);
         footerPanel.add(timeCountJours);
         footerPanel.add(timeCountYears);
 
@@ -96,6 +100,7 @@ public class Fenetre extends JFrame implements ActionListener {
         //ajout des listeners
         newPlanet.addActionListener(this);
         timerButton.addActionListener(this);
+        exitButton.addActionListener(this);
 
         //affichage de la fenetre et arret a la fermeture
         this.setVisible(true);
@@ -110,6 +115,7 @@ public class Fenetre extends JFrame implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         //reaction aux clics sur les boutons
+        
         if (e.getSource() == newPlanet) {
             space.NewPlanet();
         } else if (e.getSource() == timerButton){
@@ -121,13 +127,22 @@ public class Fenetre extends JFrame implements ActionListener {
                 timerButton.setText("Start animation");
             }
         } else if (e.getSource()==t) {
+            
+            //refresh le compteur de temps lorsque le déclencheur est le timer 
+
             timeCountJours.setText("Jours : " + space.getTempsJours());
             timeCountYears.setText("Années : " + space.getTempsAnnées());
             space.timerPerformed();
             hud=space.getHUD();
             hud.repaint();
             mainPanel.add(hud);
-
+        
+        } else if (e.getSource() == exitButton) {
+            
+            //ferme la fenetre et kill le programme
+            
+            this.dispose();
+            System.exit(0);
         }
     }
 }
